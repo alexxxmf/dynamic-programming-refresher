d5bf0e18e812564383647858db64eb723eae63fb
alejandro <amoro@alumni.unav.es>
2021-08-08 23:24:37 +0200

    Added memoized howSum

--- how-sum.js
+++ how-sum.js
@@ -13,4 +13,24 @@ const howSum = (targetSum, numArr) => {
   return null;
 };
 
+const mHowSum = (targetSum, numArr, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  for (let num of numArr) {
+    const remainder = targetSum - num;
+    const remainderResult = mHowSum(remainder, numArr, memo);
+
+    if (remainderResult !== null) {
+      memo[targetSum] = [...remainderResult, num];
+      return memo[targetSum];
+    }
+  }
+  memo[targetSum] = null;
+  return null;
+};
+
 console.log(howSum(7, [2, 3]));
+console.log(howSum(7, [5, 3]));
+console.log(mHowSum(300, [14, 7]));
